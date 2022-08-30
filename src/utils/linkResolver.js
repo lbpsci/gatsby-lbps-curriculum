9aa0b4064dc0f07cd7d9be94ffce5f4e6a2183cb
Neil Mastroianni <suancho@gmail.com>
2022-08-30 05:35:29 -0400

    add grade_span

--- src/utils/linkResolver.js
+++ src/utils/linkResolver.js
@@ -1,5 +1,10 @@
 exports.linkResolver = doc => {
   switch (doc.type) {
+    case 'grade_span': {
+      return doc.lang === 'en-us'
+        ? `/curricula/${doc.uid}/`
+        : `/${doc.lang}/curricula/${doc.uid}/`
+    }
     case 'homepage': {
       return doc.lang === 'en-us' ? '/' : `/${doc.lang}/`
     }
